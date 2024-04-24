0ec8b6df72e52bd3e017a9447f550ba172d0d8b3
ManilShrestha <manil.shrestha2011@gmail.com>
2024-04-24 19:02:44 -0400

    Development. Rereranker Module

--- lib/EmbeddingModel.py
+++ lib/EmbeddingModel.py
@@ -6,6 +6,9 @@ class EmbeddingClient:
     """Base class for embedding models."""
     def create_embeddings(self, text_chunks):
         raise NotImplementedError("Subclasses must implement this method.")
+    
+    def bge_rerank(self, query, chunks):
+        raise NotImplementedError("Subclasses must implement this method.")
         
 
 class BlockEntropyEmbedder(EmbeddingClient):
@@ -14,11 +17,11 @@ class BlockEntropyEmbedder(EmbeddingClient):
     def __init__(self):
         load_dotenv()
         self.model = 'be-bge-embeddings'
-        self.api_key = os.getenv("BE_API_KEY")
-        self.base_url = 'https://api.blockentropy.ai/v1'
+        api_key = os.getenv("BE_API_KEY")
+        base_url = 'https://api.blockentropy.ai/v1'
 
         try:
-            self.client = OpenAI(base_url=self.base_url, api_key=self.api_key)
+            self.client = OpenAI(base_url=base_url, api_key=api_key)
         except Exception as e:
             raise ConnectionError("Failed to connect to BlockEntropy API. Please check your API key and network connection.") from e
 
@@ -51,6 +54,21 @@ class BlockEntropyEmbedder(EmbeddingClient):
         cleaned_text = text.replace("\n", " ")
         response = self.client.embeddings.create(input=[cleaned_text], model=self.model)
         return response.data[0].embedding
+    
+    def bge_rerank(self, query, chunks):
+        pairs = [[query, x] for x in chunks]
+        response = self.client.embeddings.create(
+            input= pairs,
+            model="be-bge-reranker"
+        )
+        rank_scores = response.data[0].scores
+
+        # Zip the lists together and sort by rank in descending order
+        sorted_pairs = sorted(zip(rank_scores, chunks), reverse=True, key=lambda x: x[0])
+
+        # Extract the elements from the sorted pairs
+        return zip(*sorted_pairs)
+
 
 class EmbeddingModel:
     """Facade to handle embedding models based on specified provider."""
@@ -76,3 +94,16 @@ class EmbeddingModel:
             return self.embedder.create_embeddings(texts)
         else:
             raise TypeError("Input should either be string or list of strings.")
+    
+    def bge_rerank(self, query, text_chunks):
+        """Reranks the text chunks based on the query and returns the reordered chunks for further processing
+
+        Args:
+            query: Query text
+            text_chunks (List[str]): List of text strings to be embedded.
+
+        Returns:
+            List: Reranked chunks so that highest score is at index 0
+        """
+        return self.embedder.bge_rerank(query, text_chunks)
+        
